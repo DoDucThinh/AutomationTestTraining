3821f74149eefa011d47ac15fd711ddd6b165817
Do Duc Thinh (K15 HL) <thinhddhe151091@fpt.edu.vn>
2024-12-04 13:41:46 +0700

    Thinh push them độ bao phủ cho case Search

--- Selenium/java/automation/testsuite/Tedu_CapNhapMatKhauTest.java
+++ Selenium/java/automation/testsuite/Tedu_CapNhapMatKhauTest.java
@@ -47,11 +47,34 @@ public class Tedu_CapNhapMatKhauTest extends CommonBase{
 	}
 	
 	@Test(priority = 3)
-	public void SearchSuccessfully() throws InterruptedException {
+	public void SearchSuccessfully_validValue() throws InterruptedException {
 		LoginSuccessfully();
 		teduPage.search("ASP Net");
 		Thread.sleep(3000);
 		assertTrue(driver.findElement(By.xpath("//a[text()='Bài 52: Gửi phản hồi và gửi mail trong ASP NET sử dụng SMTP']")).isDisplayed());
 	}
-
+	
+	@Test(priority = 4)
+	public void SearchSuccessfully_keywordNoResults() throws InterruptedException {
+		LoginSuccessfully();
+		teduPage.search("akm");
+		Thread.sleep(3000);
+		assertTrue(driver.findElement(By.xpath("//p[text()='Dữ liệu đang cập nhật...']")).isDisplayed());
+	}
+	
+	@Test(priority = 5)
+	public void SearchSuccessfully_emptyValue() throws InterruptedException {
+		LoginSuccessfully();
+		teduPage.search("");
+		Thread.sleep(3000);
+		assertTrue(driver.findElement(By.xpath("//label[text()='Khoá học']")).isDisplayed());
+	}
+	
+	@Test(priority = 6)
+	public void SearchSuccessfully_specialCharacters() throws InterruptedException {
+		LoginSuccessfully();
+		teduPage.search("$%^#^");
+		Thread.sleep(3000);
+		assertTrue(driver.findElement(By.xpath("//p[text()='Dữ liệu đang cập nhật...']")).isDisplayed());
+	}
 }
